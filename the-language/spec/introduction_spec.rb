7d4b0852dda0efd032c8ba4bc0fa8558d6a79ad9
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-09-26 10:04:07 +0100

    Introduction spec completed. Derp

--- the-language/spec/introduction_spec.rb
+++ the-language/spec/introduction_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe "Learning ruby" do
     end
 
     it "verifies our expectations against reality" do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to be expected
     end
 
     it "has better ways to compare equality" do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to eq expected
     end
 
     it "will ask you to fill in values" do
-      expect( 1 + 1 ).to eq __
+      expect( 1 + 1 ).to eq 2
     end
 
   end
